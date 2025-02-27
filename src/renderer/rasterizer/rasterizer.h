00e31b8937ae204e131e63129faa777245403493
oth33r <108088389+oth33r@users.noreply.github.com>
2025-02-27 23:01:53 +0300

    Rasterization

--- src/renderer/rasterizer/rasterizer.h
+++ src/renderer/rasterizer/rasterizer.h
@@ -160,7 +160,7 @@ namespace cg::renderer
 
 						if (depth_test(depth, x, y))
 						{
-							auto pixel_result = pixel_shader(vertices[0], 0.f);
+							auto pixel_result = pixel_shader(vertices[0], depth);
 							render_target->item(x, y) = RT::from_color(pixel_result);
 							depth_buffer->item(x, y) = depth;
 						}
